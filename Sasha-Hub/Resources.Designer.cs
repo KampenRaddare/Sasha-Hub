a2d03973a2bfa1a88451f7415beaa193edecbb21
Kampen Raddare <jamdycus@live.com>
2016-01-19 10:07:34 -0600

    Added SashaPurpleIcon
    Sasha's favorite color is purple so this little touch of purple will
    stay even when you change the theme.

--- Sasha-Hub/Resources.Designer.cs
+++ Sasha-Hub/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace Sasha_Hub {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap SashaPurpleIcon {
+            get {
+                object obj = ResourceManager.GetObject("SashaPurpleIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
